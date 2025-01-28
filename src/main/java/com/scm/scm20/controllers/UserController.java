873d7ae0d1b40df9646b021cb56f8c720b322342
prachi20000 <prachiyadav7324@gmail.com>
2025-01-29 00:46:40 +0530

    securing application

--- src/main/java/com/scm/scm20/controllers/UserController.java
+++ src/main/java/com/scm/scm20/controllers/UserController.java
@@ -3,33 +3,31 @@ package com.scm.scm20.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
-    //user dashboard page
-    @RequestMapping(value="/dashboard")
+    // user dashboard page
+    @RequestMapping(value = "/dashboard")
     public String userDashboard() {
         System.out.println("User dashboard");
         return "user/dashboard";
     }
 
-    //user profile page
-    @RequestMapping(value="/profile")
+    // user profile page
+    @RequestMapping(value = "/profile")
     public String userProfile() {
         System.out.println("User profile");
         return "user/profile";
     }
-    
 
-    //user add contacts page
+    // user add contacts page
 
-    //user view contacts
+    // user view contacts
 
-    //user edit contact
+    // user edit contact
 
-    //user delete contact
+    // user delete contact
 
-    //user search contact
+    // user search contact
 }
